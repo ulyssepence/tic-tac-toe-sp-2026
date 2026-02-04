2287d68c55830f6a8f9846dc4a5b4671bd4aa8b2
Ulysse Pence <git@ulyssepence.com>
2026-02-03 23:52:36 -0200

    Breaks out three.js stuff into separate file

--- src/client.tsx
+++ src/client.tsx
@@ -1,11 +1,7 @@
-import * as Fiber from '@react-three/fiber'
 import * as React from 'react'
-import * as ReactDOM from "react-dom/client";
-import * as THREE from 'three'
-import * as shader from "./shader";
-import * as game from "./game";
-
-const MESH_SCALE = 1.0
+import * as ReactDOM from "react-dom/client"
+import * as game from "./game"
+import * as render from "./render"
 
 type Message =
   | { type: 'RECEIVED_GAME_STATE', game: game.State }
@@ -15,96 +11,6 @@ function onMessage(state: game.State, message: Message): game.State {
     case 'RECEIVED_GAME_STATE':
       return message.game
   }
-}
-
-function getMaterial(isHovered: boolean) {
-  return <meshPhongMaterial color={isHovered ? 'green' : 'white'} />
-}
-
-interface HoverableProperties {
-  isHovered: boolean;
-  materialOverride?: React.ReactNode;
-}
-
-function EmptyMesh({ isHovered, materialOverride }: HoverableProperties) {
-  return (
-    <mesh>
-      <boxGeometry args={[MESH_SCALE * 0.8, MESH_SCALE * 0.8, MESH_SCALE * 0.2]} />
-      {materialOverride || getMaterial(isHovered)}
-    </mesh>
-  )
-}
-
-function OhMesh({ isHovered, materialOverride }: HoverableProperties) {
-  return (
-    <mesh rotation={[Math.PI / 2, 0, 0]}>
-      <cylinderGeometry args={[MESH_SCALE * 0.4, MESH_SCALE * 0.4, MESH_SCALE * 0.2]} />
-      {materialOverride || getMaterial(isHovered)}
-    </mesh>
-  )
-}
-
-function ExMesh({ isHovered, materialOverride }: HoverableProperties) {
-  return <>
-    <object3D rotation={[0, 0, 45]} >
-      <mesh >
-        <boxGeometry args={[MESH_SCALE * 0.2, MESH_SCALE, MESH_SCALE * 0.2]} />
-        {materialOverride || getMaterial(isHovered)}
-      </mesh>
-    </object3D>
-    <object3D rotation={[0, 0, -45]} >
-      <mesh>
-        <boxGeometry args={[MESH_SCALE * 0.2, MESH_SCALE, MESH_SCALE * 0.2]} />
-        {materialOverride || getMaterial(isHovered)}
-      </mesh>
-    </object3D>
-  </>
-}
-
-interface CellProperties {
-  coord: [number, number];
-  cell: game.Cell;
-  onClickBox: (() => void) | undefined;
-}
-function Cell({ coord, cell, onClickBox }: CellProperties) {
-  const objectRef = React.useRef<THREE.Object3D>(null);
-  const [isHovered, setHovered] = React.useState(false)
-
-  Fiber.useFrame((_, delta) => {
-  })
-
-  const bottomLeft = [
-    -MESH_SCALE * 3 / 2,
-    -MESH_SCALE * 3 / 2,
-  ]
-
-  const showHover = onClickBox ? isHovered : false
-  let mesh: React.ReactNode
-  if (cell === 'X') {
-    mesh = <ExMesh isHovered={showHover} />
-  } else if (cell === 'O') {
-    mesh = <OhMesh isHovered={showHover} />
-  } else {
-    mesh = <EmptyMesh isHovered={showHover} />
-  }
-
-  const [x, y] = coord
-  return (
-    <object3D
-      position={[
-        bottomLeft[0] + x * MESH_SCALE + 0.5 * MESH_SCALE,
-        bottomLeft[1] + y * MESH_SCALE + 0.5 * MESH_SCALE,
-        -5.5
-      ]}
-      scale={1.0}
-      onPointerOver={() => onClickBox && setHovered(true)}
-      onPointerOut={() => onClickBox && setHovered(false)}
-      onPointerUp={onClickBox}
-      ref={objectRef}
-    >
-      { mesh }
-    </object3D>
-  )
 }
 
 function View() {
@@ -143,7 +49,7 @@ function View() {
         ? () => makeMoveRemote(coord)
         : undefined
 
-      cells.push(<Cell
+      cells.push(<render.Cell
         coord={[col, row]}
         onClickBox={onClickBox}
         cell={state.board[col][row]}
@@ -151,58 +57,12 @@ function View() {
     }
   }
 
-  const winner = game.getWinner(state)
-  const playerMaterial = winner == null
-    ? <meshPhongMaterial color={'red'} />
-    : <meshPhongMaterial color={'blue'} />
-  const playerTurn =
-    <object3D
-      scale={[5, 5, 1]}
-      position={[0, 5, -10]}
-    >
-      { (winner && winner == 'X') || (!winner && state.currentPlayer === 'X')
-        ? <ExMesh isHovered={false} materialOverride={playerMaterial} />
-        : <OhMesh isHovered={false} materialOverride={playerMaterial} />
-      }
-  </object3D>
-
-  const postProcessor = `
-      float period_secs = 45.0;
-      float band_size = 0.003;
-      float band = mod(uv.y, band_size);
-      float direction = band < (band_size / 2.0) ? 1.0 : -1.2;
-
-      vec3 original = scene(uv);
-      if (original.x == 0.0 && original.y == 0.0 && original.z == 0.0) {
-        // uv = (uv + triangle(floor_to_nearest(uv.y, band_size / 4.0)) * 6.28) * 3.0;
-        uv = uv * 3.0;
-        color = vec3(
-          scene(vec2(direction * t / period_secs, 0.0) + uv + voronoi_noise(t / 4.0 + uv *  10.0) * 0.02).x,
-          scene(vec2(direction * t / period_secs, 0.0) + uv - voronoi_noise(t / 4.0 + uv *  10.0) * 0.01).y,
-          scene(vec2(direction * t / period_secs, 0.0) + uv - voronoi_noise(t / 4.0 + uv * 100.0) * 0.01).z
-        ) * 0.05;
-      } else {
-        color = vec3(
-          scene(uv + voronoi_noise(t / 4.0 + uv * 10.0) * 0.02).x,
-          scene(uv - voronoi_noise(t / 4.0 + uv * 10.0) * 0.01).y,
-          scene(uv - voronoi_noise(t / 4.0 + uv * 100.0) * 0.01).z
-        );
-      }
-  `;
-
   return (
-    <Fiber.Canvas
-      style={{ width: '100%', height: '100%' }}
-      camera={{ position: [0, 0, 0] }}
-      frameloop="always"
-    >
-      <shader.GLSLShader code={postProcessor}>
-        <ambientLight intensity={0.5} />
-        <pointLight position={[10, 10, 10]} />
-        { cells }
-        { playerTurn }
-      </shader.GLSLShader>
-    </Fiber.Canvas>
+    <render.Scene
+      winner={game.getWinner(state) || undefined}
+      currentPlayer={state.currentPlayer}
+      cells={cells}
+    />
   )
 }
 
---
+++ src/render.tsx
@@ -0,0 +1,172 @@
+import * as Fiber from '@react-three/fiber'
+import * as React from 'react'
+import * as THREE from 'three'
+import * as shader from "./shader"
+import * as game from "./game"
+
+const MESH_SCALE = 1.0
+
+function getMaterial(isHovered: boolean) {
+  return <meshPhongMaterial color={isHovered ? 'green' : 'white'} />
+}
+
+interface HoverableProperties {
+  isHovered: boolean;
+  materialOverride?: React.ReactNode;
+}
+
+function EmptyMesh({ isHovered, materialOverride }: HoverableProperties) {
+  return (
+    <mesh>
+      <boxGeometry args={[MESH_SCALE * 0.8, MESH_SCALE * 0.8, MESH_SCALE * 0.2]} />
+      {materialOverride || getMaterial(isHovered)}
+    </mesh>
+  )
+}
+
+function OhMesh({ isHovered, materialOverride }: HoverableProperties) {
+  return (
+    <mesh rotation={[Math.PI / 2, 0, 0]}>
+      <cylinderGeometry args={[MESH_SCALE * 0.4, MESH_SCALE * 0.4, MESH_SCALE * 0.2]} />
+      {materialOverride || getMaterial(isHovered)}
+    </mesh>
+  )
+}
+
+function ExMesh({ isHovered, materialOverride }: HoverableProperties) {
+  return <>
+    <object3D rotation={[0, 0, 45]} >
+      <mesh >
+        <boxGeometry args={[MESH_SCALE * 0.2, MESH_SCALE, MESH_SCALE * 0.2]} />
+        {materialOverride || getMaterial(isHovered)}
+      </mesh>
+    </object3D>
+    <object3D rotation={[0, 0, -45]} >
+      <mesh>
+        <boxGeometry args={[MESH_SCALE * 0.2, MESH_SCALE, MESH_SCALE * 0.2]} />
+        {materialOverride || getMaterial(isHovered)}
+      </mesh>
+    </object3D>
+  </>
+}
+
+interface CellProperties {
+  coord: [number, number];
+  cell: game.Cell;
+  onClickBox: (() => void) | undefined;
+}
+export function Cell({ coord, cell, onClickBox }: CellProperties) {
+  const objectRef = React.useRef<THREE.Object3D>(null);
+  const [isHovered, setHovered] = React.useState(false)
+
+  Fiber.useFrame((_, delta) => {
+  })
+
+  const bottomLeft = [
+    -MESH_SCALE * 3 / 2,
+    -MESH_SCALE * 3 / 2,
+  ]
+
+  const showHover = onClickBox ? isHovered : false
+  let mesh: React.ReactNode
+  if (cell === 'X') {
+    mesh = <ExMesh isHovered={showHover} />
+  } else if (cell === 'O') {
+    mesh = <OhMesh isHovered={showHover} />
+  } else {
+    mesh = <EmptyMesh isHovered={showHover} />
+  }
+
+  const [x, y] = coord
+  return (
+    <object3D
+      position={[
+        bottomLeft[0] + x * MESH_SCALE + 0.5 * MESH_SCALE,
+        bottomLeft[1] + y * MESH_SCALE + 0.5 * MESH_SCALE,
+        -5.5
+      ]}
+      scale={1.0}
+      onPointerOver={() => onClickBox && setHovered(true)}
+      onPointerOut={() => onClickBox && setHovered(false)}
+      onPointerUp={onClickBox}
+      ref={objectRef}
+    >
+      { mesh }
+    </object3D>
+  )
+}
+
+export interface SceneProperties {
+  currentPlayer: game.Player;
+  winner?: game.Player;
+  cells: Array<React.ReactNode>,
+}
+export function Scene({ currentPlayer, winner, cells }: SceneProperties) {
+  const playerMaterial = winner == null
+    ? <meshPhongMaterial color={'red'} />
+    : <meshPhongMaterial color={'blue'} />
+
+  const postProcessor = `
+      float period_secs = 45.0;
+      float band_size = 0.003;
+      float band = mod(uv.y, band_size);
+      float direction = band < (band_size / 2.0) ? 1.0 : -1.2;
+
+      vec3 original = scene(uv);
+      if (original.x == 0.0 && original.y == 0.0 && original.z == 0.0) {
+        // uv = (uv + triangle(floor_to_nearest(uv.y, band_size / 4.0)) * 6.28) * 3.0;
+        uv = uv * 3.0;
+        color = vec3(
+          scene(vec2(direction * t / period_secs, 0.0) + uv + voronoi_noise(t / 4.0 + uv *  10.0) * 0.02).x,
+          scene(vec2(direction * t / period_secs, 0.0) + uv - voronoi_noise(t / 4.0 + uv *  10.0) * 0.01).y,
+          scene(vec2(direction * t / period_secs, 0.0) + uv - voronoi_noise(t / 4.0 + uv * 100.0) * 0.01).z
+        ) * 0.05;
+      } else {
+        color = vec3(
+          scene(uv + voronoi_noise(t / 4.0 + uv * 10.0) * 0.02).x,
+          scene(uv - voronoi_noise(t / 4.0 + uv * 10.0) * 0.01).y,
+          scene(uv - voronoi_noise(t / 4.0 + uv * 100.0) * 0.01).z
+        );
+      }
+  `
+
+  return (
+    <Fiber.Canvas
+      style={{ width: '100%', height: '100%' }}
+      camera={{ position: [0, 0, 0] }}
+      frameloop="always"
+    >
+      <shader.GLSLShader code={postProcessor}>
+        <ambientLight intensity={0.5} />
+        <pointLight position={[10, 10, 10]} />
+
+        { cells }
+
+        <PlayerTurn currentPlayer={currentPlayer} winner={winner} />
+      </shader.GLSLShader>
+    </Fiber.Canvas>
+  )
+}
+
+
+export interface PlayerTurnProperties {
+  currentPlayer: game.Player;
+  winner?: game.Player;
+}
+export function PlayerTurn({ currentPlayer, winner }: PlayerTurnProperties) {
+  const playerMaterial = winner == null
+    ? <meshPhongMaterial color={'red'} />
+    : <meshPhongMaterial color={'blue'} />
+
+  return (
+    <object3D
+      scale={[5, 5, 1]}
+      position={[0, 5, -10]}
+    >
+      { (winner && winner == 'X') || (!winner && currentPlayer === 'X')
+        ? <ExMesh isHovered={false} materialOverride={playerMaterial} />
+        : <OhMesh isHovered={false} materialOverride={playerMaterial} />
+      }
+    </object3D>
+  )
+}
