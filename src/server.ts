c7c8636bc0ec5e6e9a8d6f2433da27ee88f6094e
Ulysse Pence <git@ulyssepence.com>
2026-02-03 23:21:22 -0200

    Migrates to esbuild

--- src/server.ts
+++ src/server.ts
@@ -1,37 +1,27 @@
 import express from 'express'
 import http from 'http'
 import path  from 'path'
-import * as vite from 'vite-express'
-import * as ttt from './tic-tac-toe'
+import * as game from './game'
 
-let state = ttt.createGame()
+let state = game.createGame()
 
 const app = express()
 app.use(express.json())
 
 app.get('/', (req, res) => {
-  res.sendFile(path.resolve('assets/index.html'))
+  res.sendFile(path.resolve('static/index.html'))
 })
 
 app.get('/game', (req, res) => {
-    res.json({ result: state })
-})
-
-app.post('/move', (req, res) => {
-    try {
-        if (!Object.hasOwn(req.body, 'position')) {
-            res.json({ type: 'ERROR', error: 'Missing position' })
-        }
-
-        state = ttt.makeMove(state, req.body.position)
-    } catch (e) {
-        res.json({ type: 'ERROR', error: (e as Error).message })
-    }
-
     res.json({ type: 'SUCCESS', result: state })
 })
 
-app.use(express.static('./assets/'))
-
-vite.listen(app, 8000, () => {
+app.post('/move', (req, res) => {
+    state = game.makeMove(state, req.body.coord)
+    res.json({ type: 'SUCCESS', result: state })
 })
+
+app.use(express.static('./'))
+
+const server = http.createServer(app)
+server.listen(8000, '0.0.0.0')
